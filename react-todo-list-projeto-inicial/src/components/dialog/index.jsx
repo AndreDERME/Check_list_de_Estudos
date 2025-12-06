bc62993d456e4c6adfafb210b75d93becd0dec09
Andre Derme <andredermee@gmail.com>
2025-12-05 21:11:17 -0300

    Update index.jsx

--- react-todo-list-projeto-inicial/src/components/dialog/index.jsx
+++ react-todo-list-projeto-inicial/src/components/dialog/index.jsx
@@ -1,3 +1,12 @@
-export function Dialog (){
-    
+import React from "react";
+
+export function Dialog() {
+  return (
+    <React.Fragment>
+      <dialog>
+        <button autoFocus>Close</button>
+        <p>esse modal dialog esta</p>
+      </dialog>
+    </React.Fragment>
+  );
 }
